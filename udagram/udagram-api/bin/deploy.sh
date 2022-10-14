9f85e1543f11b5ad4810f783c5bf7a9e922fbb18
nadeenay <70846138+nadeenay@users.noreply.github.com>
2022-10-14 14:06:53 +0200

    Edit in scripts

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,2 +1,3 @@
+eb init udagram-api --platform node.js --region us-east-1 &&
 eb use udagram-api-env && eb setenv JWT_SECRET=$JWT_SECRET POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
-eb deploy   
+eb deploy udagram-api-env   

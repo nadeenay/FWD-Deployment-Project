5b8fa0926730059c61900e27e32810c394c656a0
nadeenay <70846138+nadeenay@users.noreply.github.com>
2022-10-14 16:31:26 +0200

    Edit in scripts

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagram-b/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mudagram-b/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-b/
